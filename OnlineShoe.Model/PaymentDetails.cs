a56689a4ea8825df054b4ab0fe609550816653d6
Olasile Oyedipe <oyedipe63@gmail.com>
2024-01-05 15:49:12 +0100

    Payment and shipping model

---
+++ OnlineShoe.Model/PaymentDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShoe.Model
+{
+    public class PaymentDetails
+    {
+        public int Id { get; set; }
+        public Order? Order { get; set; }
+        public int OrderId { get; set; }
+        public int CardNumber { get; set; }
+        public string CardHolderName { get; set; }
+        public DateTime  ExpiryDate { get; set; }
+        public int CVV { get; set; }    
+        public string PaymentStatus { get; set; }
+    }
+}
---
+++ OnlineShoe.Model/ShippingInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShoe.Model
+{
+    public class ShippingInfo
+    {
+        public int Id { get; set; }
+        public Order? Order { get; set; }
+        public int OrderId { get; set; }
+        public string RecipeintName { get; set; }
+        public  string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string  ShippingStatus {  get; set; }
+
+    }
+}
